81a8333b1622868940f265acb7821024608deb7f
Kshitij Gupta <kshitijgm@gmail.com>
2018-11-01 18:10:42 +0000

    beryllium: Build kraken
    Change-Id: I45f97a64b4f9734c8e524fc1c677808c361be190
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@ TARGET_OTA_ASSERT_DEVICE := beryllium
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := beryllium_defconfig
+TARGET_KERNEL_CONFIG := kraken_user_defconfig
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-gnu-8.2.1/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
