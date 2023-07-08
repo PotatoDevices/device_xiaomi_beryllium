51c83e997f263426b2ff7ae143b1d1ad8682c1a2
SahilSonar <sss.sonar2003@gmail.com>
2023-07-08 11:05:33 +0200

    beryllium: s/lineage/potato

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/potato_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beryllium-user \
-    lineage_beryllium-userdebug \
-    lineage_beryllium-eng
+    potato_beryllium-user \
+    potato_beryllium-userdebug \
+    potato_beryllium-eng
